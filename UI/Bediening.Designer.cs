6908c9afa836219a8da81b63c2cba39315cadbe1
bramGroen <bramtgroen@gmail.com>
2017-06-08 13:24:40 +0200

    Form Bediening terug
    Form bediening verdween opeens, heb opnieuw aangemaakt

--- UI/Bediening.Designer.cs
+++ UI/Bediening.Designer.cs
@@ -28,21 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Hoofdmenu = new System.Windows.Forms.Button();
+            this.btn_VerzendLunch = new System.Windows.Forms.Button();
+            this.btn_BekijkOrder = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.btn_Hoofdmenu = new System.Windows.Forms.Button();
-            this.btn_NonOrder = new System.Windows.Forms.Button();
-            this.btn_Notif_Nonalc = new System.Windows.Forms.Button();
-            this.btn_BekijkOrder = new System.Windows.Forms.Button();
-            this.btn_VerzendDrank = new System.Windows.Forms.Button();
-            this.btn_NotifDrank = new System.Windows.Forms.Button();
+            this.btn_NotifLunch = new System.Windows.Forms.Button();
             this.btn_VerzendDiner = new System.Windows.Forms.Button();
             this.btn_NotifDiner = new System.Windows.Forms.Button();
-            this.btn_VerzendLunch = new System.Windows.Forms.Button();
-            this.btn_NotifLunch = new System.Windows.Forms.Button();
+            this.btn_VerzendDrank = new System.Windows.Forms.Button();
+            this.btn_NotifDrank = new System.Windows.Forms.Button();
+            this.btn_Verzend_nonAlcohol = new System.Windows.Forms.Button();
+            this.btn_NotifNonAlcohol = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -50,17 +50,44 @@
             this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
+            // btn_Hoofdmenu
+            // 
+            this.btn_Hoofdmenu.Location = new System.Drawing.Point(22, 367);
+            this.btn_Hoofdmenu.Name = "btn_Hoofdmenu";
+            this.btn_Hoofdmenu.Size = new System.Drawing.Size(125, 47);
+            this.btn_Hoofdmenu.TabIndex = 0;
+            this.btn_Hoofdmenu.Text = "Hoofdmenu";
+            this.btn_Hoofdmenu.UseVisualStyleBackColor = true;
+            // 
+            // btn_VerzendLunch
+            // 
+            this.btn_VerzendLunch.Location = new System.Drawing.Point(6, 265);
+            this.btn_VerzendLunch.Name = "btn_VerzendLunch";
+            this.btn_VerzendLunch.Size = new System.Drawing.Size(125, 34);
+            this.btn_VerzendLunch.TabIndex = 1;
+            this.btn_VerzendLunch.Text = "Verzend Order";
+            this.btn_VerzendLunch.UseVisualStyleBackColor = true;
+            // 
+            // btn_BekijkOrder
+            // 
+            this.btn_BekijkOrder.Location = new System.Drawing.Point(153, 367);
+            this.btn_BekijkOrder.Name = "btn_BekijkOrder";
+            this.btn_BekijkOrder.Size = new System.Drawing.Size(135, 47);
+            this.btn_BekijkOrder.TabIndex = 2;
+            this.btn_BekijkOrder.Text = "Bekijk Order";
+            this.btn_BekijkOrder.UseVisualStyleBackColor = true;
+            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Location = new System.Drawing.Point(12, 30);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(273, 386);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(280, 331);
+            this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
@@ -69,7 +96,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(265, 360);
+            this.tabPage1.Size = new System.Drawing.Size(272, 305);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Lunch";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -81,7 +108,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(265, 360);
+            this.tabPage2.Size = new System.Drawing.Size(272, 305);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Diner";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -93,121 +120,94 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(265, 360);
+            this.tabPage3.Size = new System.Drawing.Size(272, 305);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Drank";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            this.tabPage4.Controls.Add(this.btn_Notif_Nonalc);
-            this.tabPage4.Controls.Add(this.btn_NonOrder);
+            this.tabPage4.Controls.Add(this.btn_NotifNonAlcohol);
+            this.tabPage4.Controls.Add(this.btn_Verzend_nonAlcohol);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(265, 360);
+            this.tabPage4.Size = new System.Drawing.Size(272, 305);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Non-alcholol Drank";
+            this.tabPage4.Text = "Non-alcohol Drank";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // btn_Hoofdmenu
+            // btn_NotifLunch
             // 
-            this.btn_Hoofdmenu.Location = new System.Drawing.Point(13, 404);
-            this.btn_Hoofdmenu.Name = "btn_Hoofdmenu";
-            this.btn_Hoofdmenu.Size = new System.Drawing.Size(121, 25);
-            this.btn_Hoofdmenu.TabIndex = 0;
-            this.btn_Hoofdmenu.Text = "Hoofdmenu";
-            this.btn_Hoofdmenu.UseVisualStyleBackColor = true;
-            // 
-            // btn_NonOrder
-            // 
-            this.btn_NonOrder.Location = new System.Drawing.Point(6, 323);
-            this.btn_NonOrder.Name = "btn_NonOrder";
-            this.btn_NonOrder.Size = new System.Drawing.Size(110, 31);
-            this.btn_NonOrder.TabIndex = 0;
-            this.btn_NonOrder.Text = "verzend order";
-            this.btn_NonOrder.UseVisualStyleBackColor = true;
-            // 
-            // btn_Notif_Nonalc
-            // 
-            this.btn_Notif_Nonalc.Location = new System.Drawing.Point(135, 323);
-            this.btn_Notif_Nonalc.Name = "btn_Notif_Nonalc";
-            this.btn_Notif_Nonalc.Size = new System.Drawing.Size(124, 31);
-            this.btn_Notif_Nonalc.TabIndex = 1;
-            this.btn_Notif_Nonalc.Text = "Notificatie";
-            this.btn_Notif_Nonalc.UseVisualStyleBackColor = true;
-            // 
-            // btn_BekijkOrder
-            // 
-            this.btn_BekijkOrder.Location = new System.Drawing.Point(165, 404);
-            this.btn_BekijkOrder.Name = "btn_BekijkOrder";
-            this.btn_BekijkOrder.Size = new System.Drawing.Size(121, 25);
-            this.btn_BekijkOrder.TabIndex = 1;
-            this.btn_BekijkOrder.Text = "Bekijk";
-            this.btn_BekijkOrder.UseVisualStyleBackColor = true;
-            // 
-            // btn_VerzendDrank
-            // 
-            this.btn_VerzendDrank.Location = new System.Drawing.Point(6, 323);
-            this.btn_VerzendDrank.Name = "btn_VerzendDrank";
-            this.btn_VerzendDrank.Size = new System.Drawing.Size(124, 31);
-            this.btn_VerzendDrank.TabIndex = 1;
-            this.btn_VerzendDrank.Text = "verzend order";
-            this.btn_VerzendDrank.UseVisualStyleBackColor = true;
-            // 
-            // btn_NotifDrank
-            // 
-            this.btn_NotifDrank.Location = new System.Drawing.Point(135, 323);
-            this.btn_NotifDrank.Name = "btn_NotifDrank";
-            this.btn_NotifDrank.Size = new System.Drawing.Size(124, 31);
-            this.btn_NotifDrank.TabIndex = 2;
-            this.btn_NotifDrank.Text = "Notificatie";
-            this.btn_NotifDrank.UseVisualStyleBackColor = true;
+            this.btn_NotifLunch.Location = new System.Drawing.Point(137, 265);
+            this.btn_NotifLunch.Name = "btn_NotifLunch";
+            this.btn_NotifLunch.Size = new System.Drawing.Size(129, 34);
+            this.btn_NotifLunch.TabIndex = 2;
+            this.btn_NotifLunch.Text = "Notificatie";
+            this.btn_NotifLunch.UseVisualStyleBackColor = true;
             // 
             // btn_VerzendDiner
             // 
-            this.btn_VerzendDiner.Location = new System.Drawing.Point(6, 323);
+            this.btn_VerzendDiner.Location = new System.Drawing.Point(6, 265);
             this.btn_VerzendDiner.Name = "btn_VerzendDiner";
-            this.btn_VerzendDiner.Size = new System.Drawing.Size(124, 31);
+            this.btn_VerzendDiner.Size = new System.Drawing.Size(125, 34);
             this.btn_VerzendDiner.TabIndex = 2;
-            this.btn_VerzendDiner.Text = "verzend order";
+            this.btn_VerzendDiner.Text = "Verzend Order";
             this.btn_VerzendDiner.UseVisualStyleBackColor = true;
             // 
             // btn_NotifDiner
             // 
-            this.btn_NotifDiner.Location = new System.Drawing.Point(135, 323);
+            this.btn_NotifDiner.Location = new System.Drawing.Point(137, 265);
             this.btn_NotifDiner.Name = "btn_NotifDiner";
-            this.btn_NotifDiner.Size = new System.Drawing.Size(124, 31);
+            this.btn_NotifDiner.Size = new System.Drawing.Size(129, 34);
             this.btn_NotifDiner.TabIndex = 3;
             this.btn_NotifDiner.Text = "Notificatie";
             this.btn_NotifDiner.UseVisualStyleBackColor = true;
             // 
-            // btn_VerzendLunch
+            // btn_VerzendDrank
             // 
-            this.btn_VerzendLunch.Location = new System.Drawing.Point(6, 323);
-            this.btn_VerzendLunch.Name = "btn_VerzendLunch";
-            this.btn_VerzendLunch.Size = new System.Drawing.Size(124, 31);
-            this.btn_VerzendLunch.TabIndex = 3;
-            this.btn_VerzendLunch.Text = "verzend order";
-            this.btn_VerzendLunch.UseVisualStyleBackColor = true;
+            this.btn_VerzendDrank.Location = new System.Drawing.Point(6, 265);
+            this.btn_VerzendDrank.Name = "btn_VerzendDrank";
+            this.btn_VerzendDrank.Size = new System.Drawing.Size(125, 34);
+            this.btn_VerzendDrank.TabIndex = 2;
+            this.btn_VerzendDrank.Text = "Verzend Order";
+            this.btn_VerzendDrank.UseVisualStyleBackColor = true;
             // 
-            // btn_NotifLunch
+            // btn_NotifDrank
             // 
-            this.btn_NotifLunch.Location = new System.Drawing.Point(135, 323);
-            this.btn_NotifLunch.Name = "btn_NotifLunch";
-            this.btn_NotifLunch.Size = new System.Drawing.Size(124, 31);
-            this.btn_NotifLunch.TabIndex = 4;
-            this.btn_NotifLunch.Text = "Notificatie";
-            this.btn_NotifLunch.UseVisualStyleBackColor = true;
+            this.btn_NotifDrank.Location = new System.Drawing.Point(137, 265);
+            this.btn_NotifDrank.Name = "btn_NotifDrank";
+            this.btn_NotifDrank.Size = new System.Drawing.Size(129, 34);
+            this.btn_NotifDrank.TabIndex = 3;
+            this.btn_NotifDrank.Text = "Notificatie";
+            this.btn_NotifDrank.UseVisualStyleBackColor = true;
+            // 
+            // btn_Verzend_nonAlcohol
+            // 
+            this.btn_Verzend_nonAlcohol.Location = new System.Drawing.Point(6, 265);
+            this.btn_Verzend_nonAlcohol.Name = "btn_Verzend_nonAlcohol";
+            this.btn_Verzend_nonAlcohol.Size = new System.Drawing.Size(125, 34);
+            this.btn_Verzend_nonAlcohol.TabIndex = 2;
+            this.btn_Verzend_nonAlcohol.Text = "Verzend Order";
+            this.btn_Verzend_nonAlcohol.UseVisualStyleBackColor = true;
+            // 
+            // btn_NotifNonAlcohol
+            // 
+            this.btn_NotifNonAlcohol.Location = new System.Drawing.Point(137, 265);
+            this.btn_NotifNonAlcohol.Name = "btn_NotifNonAlcohol";
+            this.btn_NotifNonAlcohol.Size = new System.Drawing.Size(129, 34);
+            this.btn_NotifNonAlcohol.TabIndex = 3;
+            this.btn_NotifNonAlcohol.Text = "Notificatie";
+            this.btn_NotifNonAlcohol.UseVisualStyleBackColor = true;
             // 
             // Bediening
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.btn_BekijkOrder);
             this.Controls.Add(this.btn_Hoofdmenu);
-            this.Controls.Add(this.tabControl1);
             this.Name = "Bediening";
             this.Text = "Bediening";
             this.tabControl1.ResumeLayout(false);
@@ -221,20 +221,20 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btn_Hoofdmenu;
+        private System.Windows.Forms.Button btn_VerzendLunch;
+        private System.Windows.Forms.Button btn_BekijkOrder;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.Button btn_NonOrder;
-        private System.Windows.Forms.Button btn_Hoofdmenu;
         private System.Windows.Forms.Button btn_NotifLunch;
-        private System.Windows.Forms.Button btn_VerzendLunch;
+        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button btn_NotifDiner;
         private System.Windows.Forms.Button btn_VerzendDiner;
+        private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Button btn_NotifDrank;
         private System.Windows.Forms.Button btn_VerzendDrank;
-        private System.Windows.Forms.Button btn_Notif_Nonalc;
-        private System.Windows.Forms.Button btn_BekijkOrder;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button btn_NotifNonAlcohol;
+        private System.Windows.Forms.Button btn_Verzend_nonAlcohol;
     }
 }
