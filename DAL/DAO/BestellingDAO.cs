fc5d563289284dda5ab420b8cd3b8f017c159db5
Charlène <charleneram@hotmail.com>
2017-06-08 11:53:38 +0100

    Veranderd3

--- DAL/DAO/BestellingDAO.cs
+++ DAL/DAO/BestellingDAO.cs
@@ -11,32 +11,31 @@ namespace DAL
     public class BestellingDAO
     {
         private SqlConnection conn = DbConnection.GetSqlConnection();
-    }
 
-    public List<Bestelling> GetAll()
-    {
-        return null;
-    }
+        public List<Bestelling> GetAll()
+        {
+            return null;
+        }
 
-    public Bestelling GetForId(int Id)
-    {
-        return null;
-    }
+        public Bestelling GetForId(int Id)
+        {
+            return null;
+        }
 
-    public bool Create(Bestelling Object)
-    {
-        return false;
-    }
+        public bool Create(Bestelling Object)
+        {
+            return false;
+        }
 
-    public bool Update(Bestelling Object)
-    {
-        return false;
-    }
+        public bool Update(Bestelling Object)
+        {
+            return false;
+        }
 
-    public bool Delete(Bestelling Object)
-    {
-        return false;
-    }
+        public bool Delete(Bestelling Object)
+        {
+            return false;
+        }
 
-}
+    }
 }
