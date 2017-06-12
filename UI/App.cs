367f63bc49fc02c618d1b4f20e0baf63750beff7
Nick Versteeg <nick.versteeg@outlook.com>
2017-06-12 14:38:01 +0200

    Merge Bram Into master

--- UI/App.cs
+++ UI/App.cs
@@ -16,7 +16,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TafelOverzicht());
+            Application.Run(new MainForm());
         }
     }
 }
