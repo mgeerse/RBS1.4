45e8bd937e7a1dac27f7d7704dd46db1da16869a
Maarten Geerse <582297@student.inholland.nl>
2017-06-17 16:48:34 +0200

    Barman af, staat in kok? Ik weet het ben heel fout bezig!
    Moet nu alleen de kok zien uit te vogelen om zo makkelijk mogelijk te kopieren.

---
+++ StyleGuide/Algemene Stijl/Bar en Kok/Landschap Style.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Algemene_Stijl.Bar_en_Kok
+{
+    class Landschap_Style
+    {
+    }
+}
