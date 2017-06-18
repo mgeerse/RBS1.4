838ed896f259e7d9cb6a2072fb1889c34c49d2ad
Nick Versteeg <nick.versteeg@outlook.com>
2017-06-18 22:06:41 +0200

    Waarom hadden we nog knoppen voor voorraad 🤔

--- UI/Bediening/BedieningTafelOverzichtForm.Designer.cs
+++ UI/Bediening/BedieningTafelOverzichtForm.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_KeukenVoorraad = new System.Windows.Forms.Button();
-            this.btn_BarVoorraad = new System.Windows.Forms.Button();
             this.Btn_Tafel1 = new System.Windows.Forms.Button();
             this.btn_Tafel2 = new System.Windows.Forms.Button();
             this.btn_Tafel3 = new System.Windows.Forms.Button();
@@ -46,31 +44,11 @@
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btn_KeukenVoorraad
-            // 
-            this.btn_KeukenVoorraad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_KeukenVoorraad.Location = new System.Drawing.Point(3, 3);
-            this.btn_KeukenVoorraad.Name = "btn_KeukenVoorraad";
-            this.btn_KeukenVoorraad.Size = new System.Drawing.Size(225, 100);
-            this.btn_KeukenVoorraad.TabIndex = 0;
-            this.btn_KeukenVoorraad.Text = "KeukenVoorraad";
-            this.btn_KeukenVoorraad.UseVisualStyleBackColor = true;
-            // 
-            // btn_BarVoorraad
-            // 
-            this.btn_BarVoorraad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_BarVoorraad.Location = new System.Drawing.Point(234, 3);
-            this.btn_BarVoorraad.Name = "btn_BarVoorraad";
-            this.btn_BarVoorraad.Size = new System.Drawing.Size(225, 100);
-            this.btn_BarVoorraad.TabIndex = 1;
-            this.btn_BarVoorraad.Text = "BarVoorraad";
-            this.btn_BarVoorraad.UseVisualStyleBackColor = true;
-            // 
             // Btn_Tafel1
             // 
             this.Btn_Tafel1.BackColor = System.Drawing.Color.Lime;
             this.Btn_Tafel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_Tafel1.Location = new System.Drawing.Point(3, 165);
+            this.Btn_Tafel1.Location = new System.Drawing.Point(3, 59);
             this.Btn_Tafel1.Name = "Btn_Tafel1";
             this.Btn_Tafel1.Size = new System.Drawing.Size(225, 115);
             this.Btn_Tafel1.TabIndex = 2;
@@ -82,7 +60,7 @@
             // 
             this.btn_Tafel2.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel2.Location = new System.Drawing.Point(234, 165);
+            this.btn_Tafel2.Location = new System.Drawing.Point(234, 59);
             this.btn_Tafel2.Name = "btn_Tafel2";
             this.btn_Tafel2.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel2.TabIndex = 3;
@@ -94,7 +72,7 @@
             // 
             this.btn_Tafel3.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel3.Location = new System.Drawing.Point(3, 286);
+            this.btn_Tafel3.Location = new System.Drawing.Point(3, 180);
             this.btn_Tafel3.Name = "btn_Tafel3";
             this.btn_Tafel3.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel3.TabIndex = 4;
@@ -106,7 +84,7 @@
             // 
             this.btn_Tafel4.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel4.Location = new System.Drawing.Point(234, 286);
+            this.btn_Tafel4.Location = new System.Drawing.Point(234, 180);
             this.btn_Tafel4.Name = "btn_Tafel4";
             this.btn_Tafel4.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel4.TabIndex = 5;
@@ -118,7 +96,7 @@
             // 
             this.btn_Tafel5.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel5.Location = new System.Drawing.Point(3, 407);
+            this.btn_Tafel5.Location = new System.Drawing.Point(3, 301);
             this.btn_Tafel5.Name = "btn_Tafel5";
             this.btn_Tafel5.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel5.TabIndex = 6;
@@ -130,7 +108,7 @@
             // 
             this.btn_Tafel6.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel6.Location = new System.Drawing.Point(234, 407);
+            this.btn_Tafel6.Location = new System.Drawing.Point(234, 301);
             this.btn_Tafel6.Name = "btn_Tafel6";
             this.btn_Tafel6.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel6.TabIndex = 7;
@@ -142,7 +120,7 @@
             // 
             this.btn_Tafel7.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel7.Location = new System.Drawing.Point(3, 528);
+            this.btn_Tafel7.Location = new System.Drawing.Point(3, 422);
             this.btn_Tafel7.Name = "btn_Tafel7";
             this.btn_Tafel7.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel7.TabIndex = 8;
@@ -154,7 +132,7 @@
             // 
             this.btn_Tafel8.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel8.Location = new System.Drawing.Point(234, 528);
+            this.btn_Tafel8.Location = new System.Drawing.Point(234, 422);
             this.btn_Tafel8.Name = "btn_Tafel8";
             this.btn_Tafel8.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel8.TabIndex = 9;
@@ -166,7 +144,7 @@
             // 
             this.btn_Tafel9.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel9.Location = new System.Drawing.Point(3, 649);
+            this.btn_Tafel9.Location = new System.Drawing.Point(3, 543);
             this.btn_Tafel9.Name = "btn_Tafel9";
             this.btn_Tafel9.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel9.TabIndex = 10;
@@ -178,7 +156,7 @@
             // 
             this.btn_Tafel10.BackColor = System.Drawing.Color.Lime;
             this.btn_Tafel10.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F);
-            this.btn_Tafel10.Location = new System.Drawing.Point(234, 649);
+            this.btn_Tafel10.Location = new System.Drawing.Point(234, 543);
             this.btn_Tafel10.Name = "btn_Tafel10";
             this.btn_Tafel10.Size = new System.Drawing.Size(225, 115);
             this.btn_Tafel10.TabIndex = 11;
@@ -190,7 +168,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Lime;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(3, 109);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(225, 50);
             this.button1.TabIndex = 12;
@@ -201,7 +179,7 @@
             // 
             this.btn_Bezet.BackColor = System.Drawing.Color.Red;
             this.btn_Bezet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Bezet.Location = new System.Drawing.Point(234, 109);
+            this.btn_Bezet.Location = new System.Drawing.Point(234, 3);
             this.btn_Bezet.Name = "btn_Bezet";
             this.btn_Bezet.Size = new System.Drawing.Size(225, 50);
             this.btn_Bezet.TabIndex = 13;
@@ -211,8 +189,6 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.btn_KeukenVoorraad);
-            this.flowLayoutPanel1.Controls.Add(this.btn_BarVoorraad);
             this.flowLayoutPanel1.Controls.Add(this.button1);
             this.flowLayoutPanel1.Controls.Add(this.btn_Bezet);
             this.flowLayoutPanel1.Controls.Add(this.Btn_Tafel1);
@@ -246,9 +222,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_KeukenVoorraad;
-        private System.Windows.Forms.Button btn_BarVoorraad;
         private System.Windows.Forms.Button Btn_Tafel1;
         private System.Windows.Forms.Button btn_Tafel2;
         private System.Windows.Forms.Button btn_Tafel3;
